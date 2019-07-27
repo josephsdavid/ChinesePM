8e8464c458640db1551280e288b5e6585be43183
josephsdavid <josephsd@smu.edu>
2019-07-27 02:04:24 -0500

    added daily smoothing so i dont die

--- R/EDA.R
+++ R/EDA.R
@@ -1,2 +1,26 @@
+library(tswgewrapped)
+library(ggplot2)
 source("preprocessing.R")
+dta <- preprocess('../data/')
+ls(dta)
+# [1] "BeijingPM_"   "ChengduPM_"   "GuangzhouPM_" "ShanghaiPM_"  "ShenyangPM_" 
+beijing <- dta$BeijingPM_
+names(beijing)
+#  [1] "No"              "year"            "month"           "day"            
+#  [5] "hour"            "season"          "PM_Dongsi"       "PM_Dongsihuan"  
+#  [9] "PM_Nongzhanguan" "PM_US Post"      "DEWP"            "HUMI"           
+# [13] "PRES"            "TEMP"            "cbwd"            "Iws"            
+# [17] "precipitation"   "Iprec"          
+bjts <- beijing$PM_Nongzhanguan
+bjts %>>% forecast::tsclean() %>>% abs -> bjts
+plot(decompose(bjts))
+plotts.sample.wge(bjts)
+# this shows the same thing
+forecast::gglagplot(bjts)
+forecast::ggAcf(bjts)
+par(mfrow=c(1,1))
+acf(bjts)
+difference(arima, bjts, 1) # this is because I imputed so many NAs, the last 30,000 values should be good
+difference(arima, abs( forecast::tsclean( beijing$PM_US ) ),1)
 
+aic5(bjts, silent = F)
